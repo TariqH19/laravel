8c1caea70b71333b50e2754bc35150e265247162
Tariq Horan <N00202437@iadt.ie>
2022-11-16 16:07:10 +0000

    The user function returns a user profile so a token is required to view it, The logout function requires a token to make sure it is the right user, when logout is successful a success message is returned, routing to the sanctum software that handles our token functionality, the routes that are in the object require a token the code is in the auth controller, if you are logged in you have crud functionality if not you only have read permissions

--- movies/routes/api.php
+++ movies/routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\MovieController;
+use App\Http\Controllers\PublisherController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\MovieController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,8 +17,37 @@ use App\Http\Controllers\MovieController;
 |
 */
 
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-//Unlike the wep.php this is stateless it doesnt need an old requests to work every request works in isolation
-Route::apiResource('/movies', MovieController::Class);
+
+Route::post('/auth/register', [AuthController::class, 'register']);
+Route::post('/auth/login', [AuthController::class, 'login']);
+
+// every route within these curly brackets require authentication token
+Route::middleware('auth:sanctum')->group(function (){
+    Route::post('/auth/logout',[AuthController::class, 'logout']);
+    Route::get('/auth/user',[AuthController::class, 'user']);
+
+    // You need to be logged in for all movie functionality except get all and get by id
+    Route::apiResource('/movies', MovieController::class)->except((['index', 'show']));
+});
+
+// This one line creates all routes for the MovieController
+// if you are not providing all actions the you don't need all the routes
+// so you can specfiy the routes individually
+//Route::apiResource('/movies', MovieController::class);
+
+// Movies - Define the get all and get by ID routes outside the authentication group
+Route::get('/movies', [MovieController::class, 'index']);
+Route::get('/movies/{movie}', [MovieController::class, 'show']);
+
+
+// this line create all routes for PublisherController.
+// These routes can all be accessed without an authentication token.
+// How would you alter this so you need authentication for publisher routes that alter the database?
+Route::apiResource('/cinemas', PublisherController::class);
+
+// This will define the route for Author, the implementation is not yet completed, so it's commented out.
+// Route::resource('/authors', AuthorController::class)->only(['index', 'show']);
