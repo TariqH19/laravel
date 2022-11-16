1fba75564933556a0a636f5fe374f314a464b719
Tariq Horan <N00202437@iadt.ie>
2022-11-16 14:43:15 +0000

    added the belongs to function to make each movie belong to a cinema, the cinema_id is also now in the fillable so it is stored.

--- movies/app/Models/Movie.php
+++ movies/app/Models/Movie.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Models\Movie;
+use App\Models\Cinema;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
@@ -11,5 +12,11 @@ class Movie extends Model
     use HasFactory;
     //This is what needs to be filled if it isn't there will be an error
     //This also prevents mass assignment, this is when a collumn in your database is changed that you didn't expect
-    protected $fillable = ['title','genre','runtime','director','rating','description','release_date','image'];
+    protected $fillable = ['title','genre','runtime','director','rating','description','release_date','image','cinema_id'];
+
+public function cinema()
+    {
+        return $this->belongsTo(Cinema::class);
+    }
+
 }
